d1e4362cef85b6f3a258eb42bf45e6ef317741be
ImClean <ng.hoangson93@gmail.com>
2024-11-05 09:16:33 +0700

    push

---
+++ 1-Intro-Java/Khai-bao-va-su-dung-bien/src/VariableDataType.java
@@ -0,0 +1,18 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int i = 10; //Khai bao bien i la kieu int gia tri la 10
+        float f = 20.5f; //khai bao bien f co kieu la float gia tri la 20.5
+        double d = 20.5; // khai bao bien d co kieu la double gia tri la 20.5
+        boolean b = true;
+        char c = 'a';
+        String s = "Ha Noi";
+                System.out.println(i);
+                System.out.println(f);
+                System.out.println(d);
+                System.out.println(b);
+                System.out.println(c);
+                System.out.println(s);
+
+
+    }
+}
---
+++ 1-Intro-Java/system-time/src/Systemtime.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Systemtime {
+    //psvm + TAB để tạo nhanh hàm main()
+    public static void main(String[] args) {
+        Date now = new Date();
+        System.out.println("Now is: " + now);
+    }
+}
