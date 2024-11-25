411d4fd2c8eef6c4831e0b414c82968745bff6d8
ImClean <ng.hoangson93@gmail.com>
2024-11-25 14:29:02 +0700

    add more

---
+++ 11. DSA Stack-Queue/demo/src/gucciQueue.java
@@ -0,0 +1,9 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class gucciQueue {
+    public static void main(String[] args) {
+        Queue<String> myQueue = new LinkedList<>();
+        }
+    }
+}
---
+++ 17. IO- Binary File - Serialization/StudentBinary/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void writeDataToFile(String path, List<Student> students) {
+        try {
+            FileOutputStream fos = new FileOutputStream(path);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(students);
+            oos.close();
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
